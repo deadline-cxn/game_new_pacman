1cdbe8e9959e032754e74e69b2096e701c17e4ee
sethcoder <defectiveseth@gmail.com>
2020-04-27 23:20:57 -0400

    update wow

--- new_pacman.h
+++ new_pacman.h
@@ -5,13 +5,11 @@
 #include <string.h>
 #include <time.h>
 
-#include "dlstorm.h"
-
-#include "c_game.h"
 #include "c_entity.h"
-
+#include "c_game.h"
+#include "dlcs.h"
 
 #define SP_MAP_X 200
 #define SP_MAP_Y 200
 
-extern CLog  *Log;
+extern CLog *Log;
